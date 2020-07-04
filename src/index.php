888dcafa2e07e7d0400f68293502ae14b9dff71d
JuanAnayaR <juanayar@poligran.edu.co>
2020-07-04 14:42:31 -0500

    Integración de HTML y estilos al index

--- src/index.php
+++ src/index.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Proyecto Poli</title>
+    <link rel="icon" type="image/jpg" href="img/favicon.jpg">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
     <style type="text/css">
@@ -23,15 +25,81 @@
             $('[data-toggle="tooltip"]').tooltip();   
         });
     </script>
-</head>
 <body>
-    <div class="wrapper">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Gestion de Empleados<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Contenido HTML/acercadelproyecto.html">Acerca del proyecto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Contenido HTML/Autoresproyecto.html">Autores del proyecto</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+</BR></BR></BR>
+<div class="container">
+
+
+<div class="card">
+  <div class="card-header">
+    Proyecto Integracion Continua, Politecnico Gran colombiano
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Bienvenidos a Todos!</h5>
+    <p class="card-text">Este es nuestro proyecto para aplicar la integracion continua</p></br>
+    <p class="card-text">En esta seccion usted podra hacer la gestion de los empleados de nuestra base de datos, aca puedes podificar crear editar y eliminar, pero la funcion principal de la seccion es crear empleados</p>
+  </div>
+</div>
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/poli.png" class="d-block w-100" width="300px" height="400px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/jenkins.png" class="d-block w-100" width="300px" height="450px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/bootstrap.png" class="d-block w-100" width="300px" height="400px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/php.png" class="d-block w-100" width="300px" height="400px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/sql.png" class="d-block w-100" width="300px" height="400px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/html.JPG" class="d-block w-100" width="300px" height="400px">
+    </div>
+    <div class="carousel-item">
+      <img src="img/jquery.png" class="d-block w-100" width="300px" height="400px">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</br></br></br>
+    <div class="container">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
+            <div>
+                <div class="card">
                     <div class="page-header clearfix">
-                        <h2 class="pull-left">Empleados</h2>
-                        <a href="create.php" class="btn btn-success pull-right">Agregar nuevo empleado</a>
+                        <h2 class="pull-left">Empleados</h2><a href="create.php" class="btn btn-success">Agregar nuevo empleado</a>
+                        
                     </div>
                     <?php
                     // archivo de configuración
@@ -83,5 +151,6 @@
             </div>        
         </div>
     </div>
+</br></br></br>
 </body>
 </html>
